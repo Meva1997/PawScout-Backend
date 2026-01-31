931b8dc099954b1cf1f7a8dbbfc2138ba93b807f
Alex Medina <alexmedval2@gmail.com>
2026-01-31 12:36:38 -0600

    Add cors config to allow localhost

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import animals, volunteer, contact, adopt, users
 from app.cloudinary.routers import media
 from app.internal import admin
 from app.database import create_db_and_tables
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
